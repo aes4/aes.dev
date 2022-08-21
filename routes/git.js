62b27efd69286779bbd741fc91024e6381f5a376
Aes <aes.dev0@gmail.com>
2022-08-20 17:35:48 -0700

    webhook
    callback function

--- routes/git.js
+++ routes/git.js
@@ -2,8 +2,10 @@ var exec = require('child_process').exec;
 
 module.exports = function(server) {
     server.post('/git', function (req, res) {
-        exec('git pull');
-        exec('. .restart.sh');
+        exec('git pull', callback);
+        exec('. .restart.sh', callback);
         process.exit();
     });
 }
+
+function callback(err, stdout, stderr) { return true }
