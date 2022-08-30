5eb8011a18a332d3481df7d92d3dc11a08d86174
Aes <aes.dev0@gmail.com>
2022-08-30 03:15:00 +0000

    swap f delsel
    revisit in future

--- routes/git.js
+++ routes/git.js
@@ -2,6 +2,7 @@ var exec = require('child_process').exec;
 
 module.exports = function(server) {
     server.post('/git', function (req, res) {
+        res.sendStatus(200);  // res.end() ?
         exec('git -C ~/aes.dev pull', callback);
         exec('. ~/aes.dev/.restart.sh', callback);
         process.exit();
